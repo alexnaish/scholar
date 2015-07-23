5b847e8e879e5bc15bc488e0a0a488bb21b5548d
Iceberg <iceberg@sky.uk>
2015-07-23 16:50:24 +0100

    more tweaks to front end

--- public/app/snapshot/controller.js
+++ public/app/snapshot/controller.js
@@ -1,11 +1,13 @@
 var component = angular.module('snapshot.controller', []);
 
 component.controller("SnapshotController", ['$scope', 'SnapshotService', '$routeParams', function ($scope, SnapshotService, $routeParams) {
-
+    $scope.emptyResult = true;
     $scope.name = $routeParams.name;
     SnapshotService.listDiffs($scope.name).then(function (data) {
-        console.log('data', data);
         $scope.snapshots = data;
+        if (data && data.length > 0) {
+            $scope.emptyResult = false;
+        }
     });
 
 }]);
