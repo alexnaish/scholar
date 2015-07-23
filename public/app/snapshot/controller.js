9f7f8adbf4957eb3346d4ece57e429d5d9288272
Iceberg <iceberg@sky.uk>
2015-07-23 17:42:16 +0100

    start work on promoting candidates

--- public/app/snapshot/controller.js
+++ public/app/snapshot/controller.js
@@ -1,13 +1,26 @@
 var component = angular.module('snapshot.controller', []);
 
-component.controller("SnapshotController", ['$scope', 'SnapshotService', '$routeParams', function ($scope, SnapshotService, $routeParams) {
-    $scope.emptyResult = true;
+component.controller("SnapshotController", ['$scope', 'SnapshotService', '$routeParams', '$location', '$timeout', function ($scope, SnapshotService, $routeParams, $location, $timeout) {
     $scope.name = $routeParams.name;
+    $scope.baselined = false;
     SnapshotService.listDiffs($scope.name).then(function (data) {
         $scope.snapshots = data;
-        if (data && data.length > 0) {
-            $scope.emptyResult = false;
-        }
     });
+
+    $scope.promoteCandidate = function (candidateId) {
+        $scope.baselined = true;
+        $scope.baselinedCandidate = candidateId;
+        $timeout(function () {
+            $location.path("/baseline");
+        }, 1000);
+    };
+
+    $scope.baselinedCandidate = function (candidateId) {
+
+        console.log('$scope.baselined', $scope.baselined);
+        console.log('$scope.baselinedCandidate', $scope.baselinedCandidate);
+        console.log($scope.baselined && $scope.baselinedCandidate === candidateId);
+        return $scope.baselined && $scope.baselinedCandidate === candidateId;
+    };
 
 }]);
