1246550fc0a1d8b9a4f97fa236a56238111d289f
Alex Naish <alex.naish@bskyb.com>
2015-07-28 09:29:43 +0100

    set default port to 8080

--- api/diff/service.spec.js
+++ api/diff/service.spec.js
@@ -1,5 +1,15 @@
 describe('Diff Service', function () {
 
+    var sandbox;
+
+    beforeEach(function () {
+        sandbox = sinon.sandbox.create();
+    });
+
+    afterEach(function () {
+        sandbox.restore();
+    });
+
     it('find', function (done) {
         done();
     });
