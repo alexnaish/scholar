6ff77c881c4c49340f5d2e52ce5bd0613192f561
Iceberg <DL-NowTV-IcebergTeam@bskyb.com>
2015-09-01 14:27:44 +0100

    fix incorrect lodash remove syntax

--- public/app/snapshot/controller.js
+++ public/app/snapshot/controller.js
@@ -25,7 +25,9 @@ component.controller("SnapshotController", ['$scope', 'SnapshotService', '$route
     $scope.removeSnapshot = function (diffId) {
         console.log('should send a del request which will clear down the candidate too', diffId);
         SnapshotService.deleteSnapshot($scope.name, diffId).then(function(){
-            _.remove($scope.snapshots, '_id', diffId);
+            _.remove($scope.snapshots, {
+                _id: diffId
+            });
             toastr.info('Successfully removed candidate.', {
                 timeOut: 5 * 1000,
                 progressBar: true
