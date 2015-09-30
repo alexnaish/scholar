e3a4e97c4b0564c0bf94144099284a444582abd1
popcorn <piotr.seefeld@bskyb.com>
2015-09-30 12:52:16 +0100

    flesh out test coverage, add more integration tests

--- api/baseline/api.spec.js
+++ api/baseline/api.spec.js
@@ -1,15 +1,85 @@
+var helpers = require('../../test/setup/functions'),
+    app = require('../index'),
+    BaselineModel = require('./model'),
+    expect = require('chai').expect,
+    request = require('supertest')(app);
+
 describe('Baseline API', function () {
 
+    var assets = [
+        {
+            name: 'test-run-1',
+            data: 'somekindofbase64image'
+        },
+        {
+            name: 'test-run-2',
+            data: 'anotherbase64image'
+        },
+        {
+            name: 'test-run-3',
+            data: 'thirdbase64image'
+        }];
+    var insertedAssets;
+
+    before(function (done) {
+        helpers.removeAssets(BaselineModel, {}, function () {
+            helpers.insertAssets(BaselineModel, assets, function (results) {
+                insertedAssets = results;
+                done();
+            });
+        });
+    });
+
+    after(function (done) {
+        helpers.removeAssets(BaselineModel, {}, function () {
+            done();
+        });
+    });
+
     it('/api/baseline should list all baselines', function (done) {
-        done();
+        request.get('/api/baseline')
+            .expect('Content-Type', /json/)
+            .expect(200)
+            .end(function (err, res) {
+                expect(err).to.equal(null);
+                expect(res).to.not.equal(null);
+                expect(res.body.length).to.equal(insertedAssets.length);
+                done();
+            });
     });
 
     it('/api/baseline/:name should list all baselines with the specified name', function (done) {
-        done();
+        request.get('/api/baseline/test-run-1')
+            .expect('Content-Type', /json/)
+            .expect(200)
+            .end(function (err, res) {
+                expect(err).to.equal(null);
+                expect(res).to.not.equal(null);
+                expect(res.body).to.have.property('name', 'test-run-1');
+                done();
+            });
     });
 
     it('/api/baseline/:name/raw should render the baselined image', function (done) {
-        done();
+        var firstAsset = insertedAssets[0];
+        request.get('/api/baseline/' + firstAsset.name + '/raw')
+            .expect('Content-Type', /image/)
+            .expect(200)
+            .end(function (err, res) {
+                expect(err).to.equal(null);
+                expect(res).to.not.equal(null);
+                done();
+            });
+    });
+    
+    it('/api/baseline/:name/raw should 404 if no baseline found', function (done) {
+        request.get('/api/baseline/thisDoesntExist/raw')
+            .expect(404)
+            .end(function (err, res) {
+                expect(err).to.equal(null);
+                expect(res).to.not.equal(null);
+                done();
+            });
     });
 
 });
