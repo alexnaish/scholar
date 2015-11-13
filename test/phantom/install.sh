8bafc7bd3339bd1d27dc8534414328f1e38e0313
Iceberg <DL-NowTV-IcebergTeam@bskyb.com>
2015-11-13 13:51:08 +0000

    change phantom install script to check for executable rather than the tmp download and also clean the tmp directory

--- test/phantom/install.sh
+++ test/phantom/install.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 FILENAME=phantomjs-2.0.0-macosx.zip
-
-if [ ! -f tmp/$FILENAME ]; then
+rm -rf tmp/
+if [ ! -f phantomjs ]; then
     wget -P tmp/ https://github.com/eugene1g/phantomjs/releases/download/2.0.0-bin/$FILENAME
     unzip tmp/$FILENAME
 else
     echo 'Phantom already downloaded'
 fi
+
