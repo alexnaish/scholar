1060961fc28fe29cdfc3dbc28bc39f6923bece66
Alex Naish <lexi.drage@gmail.com>
2016-05-22 14:45:17 +0100

    Refactor baseline list functionality

--- app/js/baseline/controller.js
+++ app/js/baseline/controller.js
@@ -1,39 +1,38 @@
-(function(component) {
+(function (component) {
 
-  component.controller("BaselineController", ['BaselineService', '$routeParams', function(BaselineService, $routeParams) {
+	component.controller("BaselineController", ['BaselineService', '$routeParams', function (BaselineService, $routeParams) {
 
-    var vm = this;
-    vm.images = {};
-    var filteredByOutstanding = false;
-    var outstandingCandidates = [];
+		var vm = this;
+        vm.filtering = false;
+		var baselines = [];
+		vm.images = {};
 
-    BaselineService.listAllBaselines().then(function(data) {
-      vm.baselines = data;
-      data.forEach(function(item) {
-        if ($routeParams.refresh === item.name) {
-          vm.images[item.name] = item.raw + '?' + new Date().getTime();
-        } else {
-          vm.images[item.name] = item.raw;
-        }
-      });
-    });
+		BaselineService.listAllBaselines().then(function (data) {
+			baselines = data;
+			vm.baselines = baselines;
+			data.forEach(function (item) {
+				if ($routeParams.refresh === item.name) {
+					vm.images[item.name] = item.raw + '?' + new Date().getTime();
+				} else {
+					vm.images[item.name] = item.raw;
+				}
+			});
+		});
 
-    vm.showOutstanding = function() {
-      BaselineService.getOutstandingCandidates().then(function(data) {
-        filteredByOutstanding = true;
-        outstandingCandidates = data;
-      });
-    };
+		vm.showOutstanding = function () {
+            vm.filtering = true;
+			BaselineService.getOutstandingCandidates().then(function (outstandingCandidates) {
+				vm.baselines = _.filter(baselines, function (baseline) {
+					return outstandingCandidates.indexOf(baseline.name) !== -1;
+				})
+			});
+		};
 
-    vm.showAll = function() {
-      filteredByOutstanding = false;
-    };
+		vm.showAll = function () {
+            vm.filtering = false;
+			vm.baselines = baselines;
+		};
 
-    vm.filterByOutstanding = function(baseline) {
-      if (!filteredByOutstanding) return true;
-      return outstandingCandidates.indexOf(baseline.name) !== -1;
-    };
-
-  }]);
+	}]);
 
 })(angular.module('baseline.controller', []));
