850409323889efec7d58a27f3793ac827a390e86
Alex Naish <lexi.drage@gmail.com>
2016-06-06 23:15:05 +0100

    make error logging a little more robust

--- api/index.js
+++ api/index.js
@@ -49,6 +49,6 @@ app.get('*', function (req, res) {
     });
 });
 app.use(function(err, req, res, next) {
-  console.error('Error:', err.stack);
+  console.error('Error:', err.stack || err);
   res.status(500).json({error: 'Internal Server Error'});
 });
