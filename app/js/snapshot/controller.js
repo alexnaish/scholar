88b6b6d34dfae6fed9673ed0ac9871021dc4fdff
Alex Naish <lexi.drage@gmail.com>
2016-11-20 23:43:20 +0000

    Redirect back only if all snapshots cleared

--- app/js/snapshot/controller.js
+++ app/js/snapshot/controller.js
@@ -1,43 +1,49 @@
-(function(component) {
-  component.controller("SnapshotController", ['$scope', 'SnapshotService', '$routeParams', '$location', '$timeout', 'toastr',
-    function($scope, SnapshotService, $routeParams, $location, $timeout, toastr) {
+(function (component) {
+	component.controller("SnapshotController", ['$scope', 'SnapshotService', '$routeParams', '$location', '$timeout', 'toastr',
+		function ($scope, SnapshotService, $routeParams, $location, $timeout, toastr) {
 
-      $scope.name = $routeParams.name;
-      $scope.baselined = false;
+			$scope.name = $routeParams.name;
+			$scope.baselined = false;
 
-      SnapshotService.listDiffs($scope.name).then(function(data) {
-        $scope.snapshots = data;
-      });
+			SnapshotService.listDiffs($scope.name).then(function (data) {
+				$scope.snapshots = data;
+			});
 
-      $scope.promoteCandidate = function(candidateId) {
-        $scope.baselined = true;
-        $scope.baselinedCandidate = candidateId;
+			$scope.promoteCandidate = function (snapshot) {
+                var browser = snapshot.meta.browser;
+                SnapshotService.promoteCandidate($scope.name, snapshot.candidate).then(function () {
+                    toastr.info('Baselining: ' + browser, {
+                      timeOut: 5 * 1000,
+                      progressBar: false
+                    });
+                    $scope.snapshots = $scope.snapshots.filter(function(snapshot){
+						return snapshot.meta.browser !== browser;
+					});
+                    if($scope.snapshots.length === 0) {
+                        toastr.info('Returning to Baselines list.', {
+                          timeOut: 2 * 1000,
+                          progressBar: true
+                        });
+                        $timeout(function () {
+                            $location.url("/baseline?refresh=" + $scope.name);
+                        }, 2000);
+                    }
+				});
+			};
 
-        SnapshotService.promoteCandidate($scope.name, candidateId).then(function() {
-          $timeout(function() {
-            $location.url("/baseline?refresh=" + $scope.name);
-          }, 1000);
-        });
+			$scope.removeSnapshot = function (snapshot) {
+				SnapshotService.deleteSnapshot($scope.name, snapshot._id).then(function () {
+					_.remove($scope.snapshots, {
+						_id: snapshot._id
+					});
+					toastr.info('Successfully removed candidate.', {
+						timeOut: 5 * 1000,
+						progressBar: false
+					});
+				});
+			};
 
-      };
-
-      $scope.removeSnapshot = function(diffId) {
-        SnapshotService.deleteSnapshot($scope.name, diffId).then(function() {
-          _.remove($scope.snapshots, {
-            _id: diffId
-          });
-          toastr.info('Successfully removed candidate.', {
-            timeOut: 5 * 1000,
-            progressBar: true
-          });
-        });
-      };
-
-      $scope.isHidden = function(candidateId) {
-        return $scope.baselined && $scope.baselinedCandidate !== candidateId;
-      };
-
-    }
-  ]);
+		}
+	]);
 
 })(angular.module('snapshot.controller', []));
