1eb13cdad3f0f2c0d5af25fb2a81cdccd798a1a3
Alex Naish <alex@naish.io>
2020-07-10 10:58:05 +0100

    feat: style nav links

--- app/src/components/Navigation/index.js
+++ app/src/components/Navigation/index.js
@@ -2,13 +2,17 @@ import React, { useContext } from 'react';
 import { Link } from 'wouter';
 import { AuthContext } from '../../contexts/Auth';
 
+import './style.scss';
+
 export const Navigation = () => {
   const { user } = useContext(AuthContext);
 
   return (
-    <nav>
+    <nav className="nav">
       {
-        user ? <Link href="/dashboard"><a>Dashboard</a></Link> : <Link href="/login"><a>Login</a></Link>
+        user
+          ? <Link href="/dashboard"><a className="nav__link">Dashboard</a></Link>
+          : <Link href="/login"><a className="nav__link">Login</a></Link>
       }
     </nav>
   );
