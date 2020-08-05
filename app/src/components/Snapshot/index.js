51bb32dd227b990f544795c38b6eea4138f61d47
Alex Naish <alex@naish.io>
2020-08-05 16:17:46 +0100

    feat: nicer loading indicators for images

--- app/src/components/Snapshot/index.js
+++ app/src/components/Snapshot/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'wouter';
+import Image, { Shimmer } from 'react-shimmer';
 import PropTypes from 'prop-types';
 
 import './style.scss';
@@ -7,7 +8,11 @@ import './style.scss';
 export const Snapshot = ({ id, image_url, link, marker }) => {
   return (
     <div className="snapshot">
-      <img className="snapshot__image" src={image_url} alt={`${id} image`} />
+      <Image
+        src={image_url}
+        NativeImgProps={{ alt: `${id} image`, className: 'snapshot__image' }}
+        fallback={<Shimmer width={500} height={300} />}
+      />
       { link && <Link href={`/snapshot/${id}`}><a className="snapshot__link">{id}</a></Link> }
       { marker && <div title="Has unapproved candidates" className="snapshot__marker">?</div> }
     </div>
